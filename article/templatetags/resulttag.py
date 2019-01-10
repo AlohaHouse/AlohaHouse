0b150a9b415f5e2a78dff89456aceb2c1bc79ed8
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2019-01-10 21:50:08 +0900

    table show complete

---
+++ article/templatetags/resulttag.py
@@ -0,0 +1,16 @@
+from django import template
+register = template.Library()
+
+'''template内で
+    value = 配列名,
+    arg = 配列番号
+    を引数とする配列の値を返す
+'''
+@register.filter(name='lookup')
+def lookup(value, arg):
+    return value[arg]
+
+'''for文を配列の回数分回すために必要'''
+@register.filter(name='range')
+def filter_range(start, array):
+  return range(start, len(array))
