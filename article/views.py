3dad1943668e410b6a4b61d204459b6da2e64796
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2019-01-27 13:55:49 +0900

    searchResult

--- article/views.py
+++ article/views.py
@@ -136,6 +136,33 @@ class ResultView(TemplateView):
             arg4 = arg3.find("li", class_="cassetteitem_detail-col1").text
             addresses.append(arg4)
 
+        #交通手段取得（取得後2次元配列で保持）
+        transportation_all = []
+        transportations = [] 
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-detail")
+            arg2 = arg1.find("div", class_="cassetteitem-detail-body")
+            arg3 = arg2.find("div", class_="cassetteitem_content-body")
+            arg4 = arg3.find("li", class_="cassetteitem_detail-col2")
+            arg5 = arg4.find_all("div", class_="cassetteitem_detail-text")
+            for arg6 in arg5:
+                transportations.append(arg6.text)
+            transportation_all.append(transportations)
+            transportations = []
+
+        #築年数、階建取得（取得後配列で保持）
+        years = [] 
+        stories = []
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-detail")
+            arg2 = arg1.find("div", class_="cassetteitem-detail-body")
+            arg3 = arg2.find("div", class_="cassetteitem_content-body")
+            arg4 = arg3.find("li", class_="cassetteitem_detail-col3")
+            year = arg4.select_one("div:nth-of-type(1)").text
+            story = arg4.select_one("div:nth-of-type(2)").text
+            years.append(year)
+            stories.append(story)
+
         #建物の画像取得（取得後配列で保持）
         article_images = [] #配列の初期化
         for cassetteitem in cassetteitems:
@@ -164,8 +191,10 @@ class ResultView(TemplateView):
             article_levels = [] #1物件の階数全てを取得したので初期化
         
         #建物の賃料取得（取得後2次元配列で保持）
-        article_rent_all = [] #全ての物件の階数全てを格納(2次元配列)
-        article_rents = [] #１物件の階数全てを格納
+        article_rent_all = [] #全ての物件の賃料全てを格納(2次元配列)
+        article_rents = [] #１物件の賃料全てを格納
+        article_administration_all = [] #全ての物件の階数全てを格納(2次元配列)
+        article_administrations = [] #１物件の階数全てを格納
         for cassetteitem in cassetteitems:
             arg1 = cassetteitem.find("div", class_="cassetteitem-item")
             arg2 = cassetteitem.find_all("tbody")
@@ -176,10 +205,92 @@ class ResultView(TemplateView):
             article_rent_all.append(article_rents)
             article_rents = [] #1物件の賃料全てを取得したので初期化
 
+        #建物の管理費取得（取得後2次元配列で保持）
+        article_administration_all = [] #全ての物件の階数全てを格納(2次元配列)
+        article_administrations = [] #１物件の階数全てを格納
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-item")
+            arg2 = cassetteitem.find_all("tbody")
+            for arg3 in arg2:
+                arg4 = arg3.select_one("tr td:nth-of-type(4)") #nth-of-typeで何番目の要素なのか指定
+                arg5 = arg4.find("span", class_="cassetteitem_price--administration").text
+                article_administrations.append(arg5)
+            article_administration_all.append(article_administrations)
+            article_administrations = [] #1物件の管理費全てを取得したので初期化
+
+        #建物の敷金、礼金取得（取得後2次元配列で保持）
+        article_deposit_all = [] #全ての物件の敷金全てを格納(2次元配列)
+        article_deposits = [] #１物件の敷金全てを格納
+        article_gratuity_all = [] #全ての物件の礼金全てを格納(2次元配列)
+        article_gratuitys = [] #１物件の礼金全てを格納
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-item")
+            arg2 = cassetteitem.find_all("tbody")
+            for arg3 in arg2:
+                arg4 = arg3.select_one("tr td:nth-of-type(5)") #nth-of-typeで何番目の要素なのか指定
+                deposit = arg4.find("span", class_="cassetteitem_price--deposit").text
+                gratuity = arg4.find("span", class_="cassetteitem_price--gratuity").text
+                article_deposits.append(deposit)
+                article_gratuitys.append(gratuity)
+            article_deposit_all.append(article_deposits)
+            article_gratuity_all.append(article_gratuitys)
+            article_deposits = [] #1物件の敷金全てを取得したので初期化
+            article_gratuitys = [] #1物件の礼金全てを取得したので初期化
+
+        #間取画像取得（取得後2次元配列で保持）
+        floor_picture_all = [] #全ての間取の写真全てを格納(2次元配列)
+        floor_pictures = [] #１物件の間取の写真全てを格納
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-item")
+            arg2 = cassetteitem.find_all("tbody")
+            for arg3 in arg2:
+                arg4 = arg3.find("img")
+                # srcタグの属性を抽出
+                src = arg4.get('src')
+                floor_pictures.append(src)
+            floor_picture_all.append(floor_pictures)
+            floor_pictures = [] #1物件の階数全てを取得したので初期化
+        
+        #部屋の面積を取得（取得後2次元配列で保持）
+        article_area_all = [] #全ての物件の面積全てを格納(2次元配列)
+        article_areas = [] #１物件の面積全てを格納
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-item")
+            arg2 = cassetteitem.find_all("tbody")
+            for arg3 in arg2:
+                arg4 = arg3.select_one("tr td:nth-of-type(6)") #nth-of-typeで何番目の要素なのか指定
+                arg5 = arg4.find("span", class_="cassetteitem_menseki").text
+                text = arg5.replace('m2','')
+                article_areas.append(text)
+            article_area_all.append(article_areas)
+            article_areas = [] #1物件の階数全てを取得したので初期化
+
+        #部屋の間取を取得（取得後2次元配列で保持）
+        floor_plan_all = [] #全ての部屋の間取全てを格納(2次元配列)
+        floor_plans = [] #１部屋の間取全てを格納
+        for cassetteitem in cassetteitems:
+            arg1 = cassetteitem.find("div", class_="cassetteitem-item")
+            arg2 = cassetteitem.find_all("tbody")
+            for arg3 in arg2:
+                arg4 = arg3.select_one("tr td:nth-of-type(6)") #nth-of-typeで何番目の要素なのか指定
+                arg5 = arg4.find("span", class_="cassetteitem_madori").text
+                floor_plans.append(arg5)
+            floor_plan_all.append(floor_plans)
+            floor_plans = [] #1物件の階数全てを取得したので初期化
+
 
         context["article_names"] = article_names
         context["addresses"] = addresses
         context["article_images"] = article_images
         context["article_level_all"] = article_level_all
         context["article_rent_all"] = article_rent_all
+        context["floor_picture_all"] = floor_picture_all
+        context["article_area_all"] = article_area_all
+        context["floor_plan_all"] = floor_plan_all
+        context["article_administration_all"] = article_administration_all
+        context["article_deposit_all"] = article_deposit_all
+        context["article_gratuity_all"] = article_gratuity_all
+        context["transportation_all"] = transportation_all
+        context["years"] = years
+        context["stories"] = stories
         return context
