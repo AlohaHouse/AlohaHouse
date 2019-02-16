eebf95d535ef320c7d453c63d07d9352d8f08811
YutaSugao <mrfeather@YutaSugaonoMacBook.local>
2019-02-16 23:23:13 +0900

    詳細モーダルウィンドウ完了

---
+++ static/AlohaHouse/js/result.js
@@ -0,0 +1,67 @@
+var url = 0; // グローバル変数
+
+function getFunc(id) {
+    url = id
+}
+
+//参考https://teratail.com/questions/123821
+jQuery(function ($){
+    //モーダルウィンドウを出現させるクリックイベント
+    $(".modal-open").click( function(){
+        var target = document.getElementById("iframe");
+        var suumoUrl = "https://suumo.jp/" + url;
+        target.setAttribute("src", suumoUrl);
+
+        //キーボード操作などにより、オーバーレイが多重起動するのを防止する
+        $( this ).blur() ;    //ボタンからフォーカスを外す
+        if( $( "#modal-overlay" )[0] ) return false ;        //新しくモーダルウィンドウを起動しない
+    
+        //オーバーレイを出現させる
+        $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+        $( "#modal-overlay" ).fadeIn( "slow" ) ;
+    
+        //コンテンツをセンタリングする
+        centeringModalSyncer() ;
+    
+        //コンテンツをフェードインする
+        $( "#modal-content" ).fadeIn( "slow" ) ;
+    
+        //[#modal-overlay]、または[#modal-close]をクリックしたら…
+        $( "#modal-overlay,#modal-close" ).unbind().click( function(){
+    
+            //[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+            $( "#modal-content,#modal-overlay" ).fadeOut( "slow" , function(){
+
+                target.setAttribute("src", "");
+                //[#modal-overlay]を削除する
+                $('#modal-overlay').remove() ;
+    
+            } ) ;
+    
+        } ) ;
+    
+    } ) ;
+    
+    //リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+    $( window ).resize( centeringModalSyncer ) ;
+    
+        //センタリングを実行する関数
+        function centeringModalSyncer() {
+    
+            //画面(ウィンドウ)の幅、高さを取得
+            var w = $( window ).width() ;
+            var h = $( window ).height() ;
+    
+            // コンテンツ(#modal-content)の幅、高さを取得
+            // jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+    //        var cw = $( "#modal-content" ).outerWidth( {margin:true} );
+    //        var ch = $( "#modal-content" ).outerHeight( {margin:true} );
+            var cw = $( "#modal-content" ).outerWidth();
+            var ch = $( "#modal-content" ).outerHeight();
+    
+            //センタリングを実行する
+            $( "#modal-content" ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+    
+        }
+    
+    } ) ;
