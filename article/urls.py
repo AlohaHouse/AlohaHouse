5d7665f8ab382ae9f2c952b3057ea4066cda82b0
Masaki Ikegami <salvage0707@gmail.com>
2019-02-24 14:23:24 +0900

    修正

--- article/urls.py
+++ article/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import SearchRoutesView, ResultView, DetailView
+from .views import SearchRoutesView, ResultView
 
 
 
@@ -8,5 +8,4 @@ app_name = 'article'
 urlpatterns = [
     path('search/routes', SearchRoutesView.as_view(), name='search_routes'),
     path('result/', ResultView.as_view(), name='result'),
-    path('detail/', DetailView.as_view(), name='detail'),
 ]
